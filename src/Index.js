298b512697fb414cac36a4697d51cc922703ccfe
Aliciaasmbc <alicia.apreala@gmail.com>
2024-06-15 22:56:17 +0100

    added basic form handling and typewritter pluggin

--- src/Index.js
+++ src/Index.js
@@ -1,1 +1,13 @@
+function generateDestination(event) {
+  event.preventDefault();
 
+  new Typewriter("#destination", {
+    strings: "La France, pays de l'Europe occidentale",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let destinationFormElement = document.querySelector("#travel-generator-form");
+destinationFormElement.addEventListener("submit", generateDestination);
